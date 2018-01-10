83a4907d5606c11d8de37fe56e12dbbf63573f03
Thu Vo <31901141+ThuVo22@users.noreply.github.com>
2018-01-09 16:17:49 -0800

    Create Day27.java

---
+++ Day27.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(5);
+        System.out.println("4 3");
+        System.out.println("-1 0 4 2");
+        System.out.println("5 2");     
+        System.out.println("0 -1 2 1 4");
+        System.out.println("6 3");
+        System.out.println("1 4 -1 3 0 2");
+        System.out.println("7 2");     
+        System.out.println("0 -1 2 1 4 5 6");
+        System.out.println("10 3");
+        System.out.println("-1 3 0 2 4 5 6 7 8 9");
+
+    }
+}
